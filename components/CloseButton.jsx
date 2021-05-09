e6881f190ea9d1dec49488de0272b0e6ba4e89cd
PolCG-UPC <pol.calvache@estudiantat.upc.edu>
2021-05-09 20:18:24 +0200

    add CloseButton component

---
+++ components/CloseButton.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function CloseButton() {
+  return (
+    <div
+      style={{
+        borderRadius: 50,
+        height: 50,
+        width: 50,
+        color: 'textPrimary',
+        backgroundColor: 'textSecondary',
+      }}
+    >
+      x {/*importar svg ?*/}
+    </div>
+  );
+}
